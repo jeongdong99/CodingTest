a16c33d51f5471f88020e217d0db251eba220025
jeong dong <93365714+jeungdong@users.noreply.github.com>
2023-09-19 18:35:54 +0900

    Add files via upload

---
+++ Lv0/Baek11720/Baek11720.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Baek11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+
+        int sum = 0;
+
+        for(int a=0; a<arr.length; a++){
+            System.out.println(arr[a]);
+        }
+        for(int a=0; a<arr.length; a++){
+            System.out.println(arr[a]);
+        }
+        System.out.println(arr[0]-'0');
+
+        System.out.println(sum);
+    }
+}
