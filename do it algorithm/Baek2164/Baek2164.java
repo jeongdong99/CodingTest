3061a847501e441dc4372b7dad3e6b0bcb7e67db
jeong dong <93365714+jeungdong@users.noreply.github.com>
2023-09-30 16:53:20 +0900

    Add files via upload

---
+++ do it algorithm/Baek2164/Baek2164.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Baek2164 {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 1; i <= N; i++){
+            q.add(i);
+        }
+        while(q.size() > 1){
+            q.remove();
+            q.add(q.remove());
+        }
+        System.out.println(q.remove());
+    }
+}
